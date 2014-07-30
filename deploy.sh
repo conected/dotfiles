5186b2e81babab006b0488fdcb00a2d178850dad
Jonas Schoenherr <js@conected.de>
2014-07-30 12:37:33 +0200

    Fixed directory bugs in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -5,45 +5,45 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Init submodules
 cd ${BASEDIR}
-git submodule update
+git submodule update --init --recursive
 
 # Deploy configuration
-cd ~
+cd
 
 if [[ -d .vim ]]; then
 	mv .vim .vim.bak
 fi
-ln -sf ${BASEDIR}/.vim .vim
+ln -sf ${BASEDIR}/.vim ~/.vim
 
 if [[ -f .vimrc ]]; then
 	mv .vimrc .vimrc.bak
 fi
-ln -sf ${BASEDIR}/.vimrc .vimrc
+ln -sf ${BASEDIR}/.vimrc ~/.vimrc
 
 if [[ -f .bashrc ]]; then
 	mv .bashrc .bashrc.bak
 fi
-ln -sf ${BASEDIR}/.bashrc .bashrc
+ln -sf ${BASEDIR}/.bashrc ~/.bashrc
 
 if [[ -f .bash_aliases ]]; then
 	mv .bash_aliases .bash_aliases.bak
 fi
-ln -sf ${BASEDIR}/.bash_aliases .bash_aliases
+ln -sf ${BASEDIR}/.bash_aliases ~/.bash_aliases
 
 if [[ -f .bash_logout ]]; then
 	mv .bash_logout .bash_logout.bak
 fi
-ln -sf ${BASEDIR}/.bash_logout .bash_logout
+ln -sf ${BASEDIR}/.bash_logout ~/.bash_logout
 
 if [[ -d .oh-my-zsh ]]; then
 	mv .oh-my-zsh .oh-my-zsh.bak
 fi
-ln -sf ${BASEDIR}/.oh-my-zsh .oh-my-zsh
+ln -sf ${BASEDIR}/.oh-my-zsh ~/.oh-my-zsh
 
 if [[ -f .zshrc ]]; then
 	mv .zshrc .zshrc.bak
 fi
-ln -sf ${BASEDIR}/.zshrc .zshrc
+ln -sf ${BASEDIR}/.zshrc ~/.zshrc
 
 # Init vim plugins
 vi +PluginInstall +qall
